556a1cf4ea96709b079b15cbb97a4d39d39ca94c
Opacity975 <410281036@qq.com>
2018-07-06 11:12:14 +0800

    修改了config/index属性开启热更新

---
+++ my-vuex/src/components/synctest/box.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="hello">
+    <div class="ipt">
+      <input type="text" v-model="str">
+    </div>
+
+    <!--//word是父元素传过来的-->
+    <h2>{{ word }}</h2>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'box',
+  data () {
+    return {
+      str: ''
+    }
+  },
+  props: {
+    word: ''
+  },
+  watch: {
+    str: function (newValue, oldValue) {
+      // 每当str的值改变则发送事件update:word , 并且把值传过去
+      this.$emit('update:word', newValue)
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ my-vuex/src/components/synctest/father.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="hello">
+    <!--//input实时改变wrd的值, 并且会实时改变box里的内容-->
+    <input type="text" v-model="wrd">
+    <box :word.sync="wrd" ></box>
+  </div>
+</template>
+
+<script>
+import box from './box'
+// 引入box子组件
+export default {
+  name: 'father',
+  data () {
+    return {
+      wrd: ''
+    }
+  },
+  components: {
+    box
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
