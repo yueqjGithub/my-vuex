057339994e9a287c7da438531ffc5c6f08b6280c
Opacity975 <410281036@qq.com>
2018-07-09 15:43:45 +0800

    201807091543

--- my-vuex/src/components/synctest/father.vue
+++ my-vuex/src/components/synctest/father.vue
@@ -4,6 +4,11 @@
     <input type="text" v-model="wrd">
     <h2>{{chifan}}</h2>
     <box :word.sync="wrd" :word2.sync="chifan"></box>
+    <h3 :style="color">{{$store.state.dialog.show}}</h3>
+    <h3 :style="color">{{$store.state.dialog2.count}}</h3>
+    <button @click="$store.dispatch('add')">+</button>
+    <button @click="$store.dispatch('reduce')">-</button>
+    <input type="text" v-model="setwrd">
   </div>
 </template>
 
@@ -14,12 +19,23 @@ export default {
   name: 'father',
   data () {
     return {
-      wrd: '',
-      chifan: '吃饭原值'
+      wrd: 1,
+      chifan: '吃饭原值',
+      color: {'color': 'red'}
     }
   },
   components: {
     box
+  },
+  computed: {
+    setwrd: {
+      get: function () {
+        return this.wrd + '/' + this.$store.state.dialog2.count
+      },
+      set: function (newvalue) {
+        this.wrd += newvalue
+      }
+    }
   }
 }
 </script>
